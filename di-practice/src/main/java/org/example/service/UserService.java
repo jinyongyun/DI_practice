eee1efa125fe3fac54134c3b31dbed2e9292c71c
dbswlsdyd730@naver.com <dbswlsdyd730@naver.com>
2023-06-30 16:09:41 +0900

    initial

---
+++ di-practice/src/main/java/org/example/service/UserService.java
@@ -0,0 +1,11 @@
+package org.example.service;
+
+import org.example.annotation.Controller;
+import org.example.annotation.Inject;
+import org.example.annotation.Service;
+import org.example.controller.UserController;
+
+@Controller
+public class UserService {
+
+}
